20b5620678f3146d5c1fc87b768efecd61eff113
VictoriaAnthony <imveejo@gmail.com>
2025-10-28 12:08:46 +0000

    Create App.vue

---
+++ src/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="max-width: 400px; margin: 2rem auto; font-family: sans-serif;">
+    <h1>Vue Todo</h1>
+    <TodoList />
+  </div>
+</template>
+
+<script lang="ts">
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: { TodoList }
+};
+</script>
